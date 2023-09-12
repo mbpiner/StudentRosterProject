ca09c02fa6721d81263f72875ae28cb80e04b9c2
mbpiner <31866428+mbpiner@users.noreply.github.com>
2023-09-12 01:50:18 -0400

    Update networkstudent.cpp

--- networkstudent.cpp
+++ networkstudent.cpp
@@ -5,46 +5,36 @@ using std::cout;
 
 NetworkStudent::NetworkStudent():Student()
 {
-
     degreeType = NETWORK;
-
 }
 
 
 NetworkStudent::NetworkStudent(string studentID, string firstName, string lastName, string emailAddress, int age, int days[], DegreeType degree):Student(studentID, firstName, lastName, emailAddress, age, days)
 
 {
-
     degreeType = NETWORK;
-
 }
 
 
 DegreeType NetworkStudent::getDegreeType()
 
 {
-
     return NETWORK;
-
 }
 
 
 void NetworkStudent::print()
 
 {
-
     this->Student::print(); //call print from student class where it is only a virtual method.
     cout << "NETWORK" << "\n";
-
 }
 
 
 
 NetworkStudent::~NetworkStudent()
 {
-
     Student::~Student();
-
 }
 
 
