fa1f260a1f4acca5e168d3f97565b7a3dbb859ff
mbpiner <31866428+mbpiner@users.noreply.github.com>
2023-09-12 01:54:49 -0400

    Update student.cpp

--- student.cpp
+++ student.cpp
@@ -29,26 +29,22 @@ Student::Student(string id, string fName, string lName, string email, int age, i
 }
 
 //Getters
-    int Student::getAge() { return age; }
-    string Student::getStudentID() { return studentID; }
-    string Student::getFirstName() { return firstName; }
-    string Student::getLastName() { return lastName; }
-    string Student::getEmailAddress() { return emailAddress; }
-		int * Student::getDays(){return days;}
+int Student::getAge() { return age; }
+string Student::getStudentID() { return studentID; }
+string Student::getFirstName() { return firstName; }
+string Student::getLastName() { return lastName; }
+string Student::getEmailAddress() { return emailAddress; }
+int * Student::getDays(){return days;}
 
 
 
 //Setters
-
-
-
-
-	void Student::setAge(int age) { this->age = age; }
-	void Student::setStudentID(string studentID) { this->studentID = studentID; }
-	void Student::setFirstName(string firstName) { this->firstName = firstName; }
-	void Student::setLastName(string lastName) { this->lastName = lastName; }
-	void Student::setEmailAddress(string emailAddress) { this->emailAddress = emailAddress; }
-  void Student::setDays(int days[]) { for (int i = 0; i<3; i++)this->days[i] = days[i]; }
+void Student::setAge(int age) { this->age = age; }
+void Student::setStudentID(string studentID) { this->studentID = studentID; }
+void Student::setFirstName(string firstName) { this->firstName = firstName; }
+void Student::setLastName(string lastName) { this->lastName = lastName; }
+void Student::setEmailAddress(string emailAddress) { this->emailAddress = emailAddress; }
+void Student::setDays(int days[]) { for (int i = 0; i<3; i++)this->days[i] = days[i]; }
 
 
 //methods
@@ -56,14 +52,14 @@ Student::Student(string id, string fName, string lName, string email, int age, i
 
 void Student::print()
 {
-cout << left << setw(5) << studentID;
-cout << left << setw(15) << firstName;
-cout << left << setw(10) << lastName;
-cout << left << setw(25) << emailAddress;
-cout << left << setw(20) << age;
-cout << left << setw(10) << days[0];
-cout << left << setw(10) << days[1];
-cout << left << setw(10) << days[2];
+    cout << left << setw(5) << studentID;
+    cout << left << setw(15) << firstName;
+    cout << left << setw(10) << lastName;
+    cout << left << setw(25) << emailAddress;
+    cout << left << setw(20) << age;
+    cout << left << setw(10) << days[0];
+    cout << left << setw(10) << days[1];
+    cout << left << setw(10) << days[2];
 }
 
 
