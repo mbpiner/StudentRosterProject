965104c6a233b1b10344ba87a0472d43ec241c2a
mbpiner <31866428+mbpiner@users.noreply.github.com>
2023-09-12 01:57:51 -0400

    Update roster.h

--- roster.h
+++ roster.h
@@ -14,24 +14,16 @@ private:
     int maxRosterSize; // maximum size of roster
     Student** students;
 
-    public:
-        
-        Roster();
-        Roster(int capacity);
+public:
+    Roster();
+    Roster(int capacity);
 
-	Student* getStudentAt(int index);
-
-	void parseThenAdd(string datarow);
-
-        void printAll();
-
-        void add(string sID, string fn, string ln, string em, int age, int d1, int d2, int d3, DegreeType dt);
-
-        bool remove(string studentID);
-
-        void printByDegree(DegreeType degree);
-
-        void printDays(string studentID);
-
-        void printBadEmails();
+    Student* getStudentAt(int index);
+    void parseThenAdd(string datarow);
+    void printAll();
+    void add(string sID, string fn, string ln, string em, int age, int d1, int d2, int d3, DegreeType dt);
+    bool remove(string studentID);
+    void printByDegree(DegreeType degree);
+    void printDays(string studentID);
+    void printBadEmails();
 };
