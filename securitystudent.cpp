ddfc760ef98390072598a16dbb18044e78362454
mbpiner <31866428+mbpiner@users.noreply.github.com>
2023-09-12 01:51:06 -0400

    Update securitystudent.cpp

--- securitystudent.cpp
+++ securitystudent.cpp
@@ -4,46 +4,36 @@ using std::cout;
 
 SecurityStudent::SecurityStudent():Student()
 {
-
     degreeType = SECURITY;
-
 }
 
 
 SecurityStudent::SecurityStudent(string studentID, string firstName, string lastName, string emailAddress, int age, int days[], DegreeType degree):Student(studentID, firstName, lastName, emailAddress, age, days)
 
 {
-
     degreeType = SECURITY;
-
 }
 
 
 DegreeType SecurityStudent::getDegreeType()
 
 {
-
     return SECURITY;
-
 }
 
 
 void SecurityStudent::print()
 
 {
-
     this->Student::print(); //call print from student class where it is only a virtual method.
     cout << "SECURITY" << "\n";
-
 }
 
 
 
 SecurityStudent::~SecurityStudent()
 {
-
     Student::~Student();
-
 }
 
 
