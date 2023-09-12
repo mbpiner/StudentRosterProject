98f660d7207112de0a0397338e69c7a23dc15bcf
mbpiner <31866428+mbpiner@users.noreply.github.com>
2023-09-12 01:51:26 -0400

    Update softwarestudent.cpp

--- softwarestudent.cpp
+++ softwarestudent.cpp
@@ -4,47 +4,36 @@ using std::cout;
 
 SoftwareStudent::SoftwareStudent():Student()
 {
-
     degreeType = SOFTWARE;
-
 }
 
 
 SoftwareStudent::SoftwareStudent(string studentID, string firstName, string lastName, string emailAddress, int age, int days[], DegreeType degree):Student(studentID, firstName, lastName, emailAddress, age, days)
 
 {
-
     degreeType = SOFTWARE;
-
-
 }
 
 
 DegreeType SoftwareStudent::getDegreeType()
 
 {
-
     return degreeType;
-
 }
 
 
 void SoftwareStudent::print()
 
 {
-
     this->Student::print(); //call print from student class where it is only a virtual method.
     cout << "SOFTWARE" << "\n";
-
 }
 
 
 
 SoftwareStudent::~SoftwareStudent()
 {
-
     Student::~Student();
-
 }
 
 
