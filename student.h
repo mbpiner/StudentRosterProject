12f196b5e3d427833e6632cfb245200f1eb9fa00
mbpiner <31866428+mbpiner@users.noreply.github.com>
2023-09-12 01:53:12 -0400

    Update student.h

--- student.h
+++ student.h
@@ -24,28 +24,18 @@ public:
 
 //Setters
 
-
-
-    
-	void setStudentID(string studentID);
-	void setFirstName(string firstName);
-	void setLastName(string lastName);
-	void setEmailAddress(string emailAddress);
-	void setAge(int age);
-  void setDays(int days[]);  ///maybe days[]??
-
-  virtual void print() = 0;
-
-
-  virtual ~Student();
-
-
- 
+    void setStudentID(string studentID);
+    void setFirstName(string firstName);
+    void setLastName(string lastName);
+    void setEmailAddress(string emailAddress);
+    void setAge(int age);
+    void setDays(int days[]); 
+    virtual void print() = 0;
+    virtual ~Student();
 
 protected:
     DegreeType degreeType;
     int age;
-		int days[3];
+    int days[3];
     string studentID, firstName, lastName, emailAddress;
-
 };
