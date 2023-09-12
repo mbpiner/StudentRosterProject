76fe251ab3a9c23fd6a7713f68f69429794bdf6d
mbpiner <31866428+mbpiner@users.noreply.github.com>
2023-09-12 01:56:16 -0400

    Update student.h

--- student.h
+++ student.h
@@ -12,26 +12,26 @@ public:
 
 //Getters
 
-string getStudentID();
-string getFirstName();
-string getLastName();
-string getEmailAddress();
-int getAge();
-int* getDays();
-virtual DegreeType getDegreeType() = 0; //virtual (can't instantiate)
+    string getStudentID();
+    string getFirstName();
+    string getLastName();
+    string getEmailAddress();
+    int getAge();
+    int* getDays();
+    virtual DegreeType getDegreeType() = 0; //virtual (can't instantiate)
 
 
 
 //Setters
 
-void setStudentID(string studentID);
-void setFirstName(string firstName);
-void setLastName(string lastName);
-void setEmailAddress(string emailAddress);
-void setAge(int age);
-void setDays(int days[]); 
-virtual void print() = 0;
-virtual ~Student();
+    void setStudentID(string studentID);
+    void setFirstName(string firstName);
+    void setLastName(string lastName);
+    void setEmailAddress(string emailAddress);
+    void setAge(int age);
+    void setDays(int days[]); 
+    virtual void print() = 0;
+    virtual ~Student();
 
 protected:
     DegreeType degreeType;
